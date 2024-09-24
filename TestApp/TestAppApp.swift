cb3d654553e6182ffaf3a37c85812f294b78fe60
Dan Vitoriano <vitoriano08@gmail.com>
2024-09-24 16:06:55 -0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Danilo Vitoriano on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Danilo Vitoriano on 24/09/24.
+//
+
+import Testing
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
